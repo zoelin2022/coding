418641708fe4db8e7cec8d46f1aaa294bd0dd29f
Zoe Lin <tyzx5246@gmail.com>
2022-02-19 20:22:54 +0800

    add word count function

--- read.py
+++ read.py
@@ -4,9 +4,26 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0: # 每1000筆
-            print(len(data)) # 觀察讀取進度   
-print(f"檔案讀取完畢，總共有{len(data)}筆資料")
+        #if count % 1000 == 0: # 每1000筆
+            #print(len(data)) # 觀察讀取進度   
+#print(f"檔案讀取完畢，總共有{len(data)}筆資料")
+
+word_count = {}
+for d in data: # d = 每筆留言
+    words = d.split() # 過濾空白鍵
+    for word in words: # words = 每個單字
+        if word in word_count:
+            word_count[word] += 1 # 改變 key 的 value
+        else:
+            word_count[word] = 1 # 新增新的key進word_count字典中
+
+# 查找出現次數超過100的單字有哪些
+for word in word_count:
+    if word_count[word] > 1000000:
+        print(word, word_count[word])
+
+print(len(word_count)) # 查共有幾種單字
+print(word_count["Allen"]) # "Allen"出現過幾次
 
 total = 0
 for d in data:
@@ -26,10 +43,13 @@ for d in data:
         good.append(d)
 print(f"留言內容裡出現'good'的有{len(good)}則")
 
-# 清單快寫法
-good1 = [d for d in data if 'good' in d]
-print(f"留言內容裡出現'good'的有{len(good1)}則")
-
-bad = ['bad' in d for d in data]
-bad1 = sum('bad' in d for d in data)
-print(f"留言內容裡出現'bad'的有{len(bad1)}則")
+# 查詢單字出現次數的功能
+while True:
+    word = input("請問你想查什麼字：")
+    if word == "q":
+        break
+    if word in word_count:
+        print(f"{word}出現過的次數為{word_count[word]}")
+    else:
+        print(f"{word}出現過的次數為0")
+print("感謝使用本查詢功能")
